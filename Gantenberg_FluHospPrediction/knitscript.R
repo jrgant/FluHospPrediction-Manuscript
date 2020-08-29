85dc966a8bfe07e0e9ee0308fe3c33775e582d26
jrgant <jason.gantenberg@gmail.com>
2020-08-29 12:24:14 -0400

    sensitivity analyses incorporated, draft co-auths

--- Gantenberg_FluHospPrediction/knitscript.R
+++ Gantenberg_FluHospPrediction/knitscript.R
@@ -3,5 +3,7 @@ library(rmarkdown)
 render(
   "Gantenberg_FluHospPrediction.Rmd",
   output_format = "officedown::rdocx_document",
-  output_options = list(reference_docx = "wordref.docx")
+  output_options = list(
+    reference_docx = "wordref.docx"
+  )
 )
