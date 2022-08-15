f4f6fbcaa9e9b7b6ad606413b3fe1226c36f3c50
Jason Gantenberg <jason.gantenberg@gmail.com>
2022-08-15 14:21:48 -0400

    AJE technical review

--- Gantenberg_FluHospPrediction/knitscript.R
+++ Gantenberg_FluHospPrediction/knitscript.R
@@ -7,9 +7,9 @@ render(
   output_options = list(
     reference_docx = "wordref.docx",
     page_margins = list(
-      top = 1, 
+      top = 1,
       right = 1,
-      bottom = 1, 
+      bottom = 1,
       left = 1,
       gutter = 0,
       header = 0,
